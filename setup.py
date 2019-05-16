f2482ca4e34605f3069bc1eced04ce1bc54fd3ac
emkor93 <emkor93@gmail.com>
2019-05-16 13:04:02 +0200

    added setup.py with requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = [str(req) for req in parse_requirements(f.read())]
+
+with open("requirements-dev.txt") as f:
+    REQUIREMENTS_DEV = [str(req) for req in parse_requirements(f.read())]
+
+setup(
+    name="gmail-img-dl",
+    version="0.0.1",
+    description="CLI tool for retrieving image attachments from GMail messages (specifically from Reolink security cameras)",
+    author="Mateusz Korzeniowski",
+    author_email="emkor93@gmail.com",
+    url="https://github.com/emkor/gmail-img-dl",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': REQUIREMENTS_DEV
+    },
+    entry_points={"console_scripts": [
+        "gmail_dl = gmail_img_dl.main:cli_main"
+    ]}
+)
